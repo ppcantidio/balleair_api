d6470ed0ba2a52b61bd6838f45c88374befe94a8
pedro <ppcantidio@gmail.com>
2022-04-06 19:34:31 -0300

    delete and get flight routes

--- webapi/routes/flights.py
+++ webapi/routes/flights.py
@@ -1,8 +1,10 @@
 from webapi.models.flight import FlightModel
 from flask import Blueprint, jsonify, request
-
+from webapi.utils.database import Database
 
 flights_routes = Blueprint('flights_routes', __name__)
+
+db = Database()
 
 class FlightsRoutes:
 
@@ -27,11 +29,37 @@ class FlightsRoutes:
 
     @flights_routes.route('/', methods=['GET'])
     def get_flights():
-        pass
+        flights_list = db.select_object('flights_api', {})
 
-    @flights_routes.route('/', methods=['DELETE'])
-    def delete_flight():
-        pass
+        return jsonify({
+            'status': 'sucess',
+            'flight': flights_list
+        })
+
+    @flights_routes.route('/<flight_number>', methods=['DELETE'])
+    def delete_flight(flight_number):
+        flight_doc = db.select_one_object('flights_api', {'flightNumber': flight_number})
+
+        if flight_doc is None:
+            return jsonify({
+                'status': 'error',
+                'message': 'flight not found'
+            })
+
+        db.delete_one('flights_api', {'flightNumber': flight_number})
+
+        flight_doc = db.select_one_object('flights_api', {'flightNumber': flight_number})
+
+        if flight_doc is None:
+            return jsonify({
+                'status': 'sucess',
+                'message': 'flight deleted successfully'
+            })
+
+        return jsonify({
+            'status': 'error',
+            'message': 'error deleting flight'
+        })
 
     @flights_routes.route('/', methods=['PUT'])
     def edit_flight():
